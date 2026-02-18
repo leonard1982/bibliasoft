b832935599c308c4a614401aad2c0b9371d1e5b5
Leonardo Navarro <leo2904.trabajo@gmail.com>
2026-02-18 08:08:45 -0500

    feat: home visual refresh + facebook icon footer link

--- BIBLIASOFT/public/sw.js
+++ BIBLIASOFT/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'biblia-soft-v4';
+const CACHE_VERSION = 'biblia-soft-v5';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
@@ -20,6 +20,7 @@ const STATIC_ASSETS = [
   './assets/icons/copy.svg',
   './assets/icons/camera.svg',
   './assets/icons/download.svg',
+  './assets/icons/facebook.svg',
   './assets/icons/help.svg',
   './assets/icons/list.svg',
   './assets/icons/login.svg',
